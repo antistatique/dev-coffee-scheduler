126546d7cb25eb068d4fa5e021812f0e4b3e6b7f
Yann Gouffon <github@yago-art.ch>
2019-01-25 17:09:46 +0100

    ✨ add next speaker and some comments

--- index.js
+++ index.js
@@ -9,28 +9,45 @@ const momentFrCh = require('moment/locale/fr-ch');
 
 moment.updateLocale('fr-ch', momentFrCh);
 
+// Connect and prepare Redis Database
 const db = redis.createClient(process.env.REDIS_URL);
 const getAsync = promisify(db.get).bind(db);
 
+/**
+ * Based on the database state, send Slack message
+ *
+ * @param {setNew} Boolean set if last_speaker must be updated
+ * @param {message} String message content with NEW_SPEAKER and NEXT_SPEAKER placeholders
+ */
 const reminder = async ({ setNew = false, message }) => {
-  console.log('start reminder');
   const isActive = await getAsync('is_active');
 
   if (JSON.parse(isActive)) {
     const speakersData = await getAsync('speakers');
     const speakers = speakersData.split(' ');
     const lastSpeaker = await getAsync('last_speaker');
+
+    // Define the new speaker
     const newSpeakerId = lastSpeaker ? speakers.indexOf(lastSpeaker) + 1 : 0;
     let newSpeaker = speakers[newSpeakerId];
     if (newSpeakerId + 1 > speakers.length) newSpeaker = speakers[0];
 
+    // Define the speaker who come after the new speaker
+    let nextSpeaker = speakers[newSpeakerId + 1];
+    if (newSpeakerId + 2 > speakers.length) nextSpeaker = speakers[0];
+    if (newSpeakerId + 1 > speakers.length) nextSpeaker = speakers[1];
+
+    // Update last_speaker
     if (setNew) db.set('last_speaker', newSpeaker, redis.print);
 
+    // Post Slack message
     axios({
       method: 'post',
       url: `https://hooks.slack.com/services/${process.env.SLACK_AUTH_TOKEN}`,
       data: {
-        text: message.replace('NEW_SPEAKER', newSpeaker),
+        text: message
+          .replace('NEW_SPEAKER', newSpeaker)
+          .replace('NEXT_SPEAKER', nextSpeaker),
         channel: '#dev-coffee-scheduler',
       },
     })
@@ -43,16 +60,23 @@ const reminder = async ({ setNew = false, message }) => {
   }
 };
 
+// Monday reminder the wednesday presentation
 const mondayReminder = new schedule.RecurrenceRule();
-mondayReminder.second = 10;
+mondayReminder.dayOfWeek = 1; // Monday
+mondayReminder.hour = 10; // At 9am
+// mondayReminder.second = 10; // tests purpose
 
 schedule.scheduleJob(mondayReminder, () =>
   reminder({
     setNew: true,
-    message: `<!here|here> le ${moment()
-      .add({ days: 2 })
-      .format(
-        'LL',
-      )}, c'est au tour de *NEW_SPEAKER* de nous présenter quelque chose :male-teacher:`,
+    message: `
+      <!here|here> :warning: mercredi *${moment()
+        .add({ days: 2 })
+        .format(
+          'LL',
+        )}*, c'est à *NEW_SPEAKER* de nous présenter quelque chose :male-teacher:
+
+              :calendar: La semaine prochaine, ce sera au tour de NEXT_SPEAKER !
+      `,
   }),
 );
