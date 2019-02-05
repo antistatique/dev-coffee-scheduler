16ef98a9f80a59bc815bbfdc17e3c3eba7267ef9
Yann Gouffon <github@yago-art.ch>
2019-02-05 16:43:34 +0100

    🔧 change time

--- index.js
+++ index.js
@@ -83,8 +83,8 @@ RecurrenceRule properties
 // Monday reminder the wednesday presentation
 const mondayReminder = new schedule.RecurrenceRule();
 mondayReminder.dayOfWeek = 2; // Monday
-mondayReminder.hour = 17; // At 9am
-mondayReminder.minute = 45; // 9:01am
+mondayReminder.hour = 15; // At 9am
+mondayReminder.minute = 48; // 9:01am
 
 schedule.scheduleJob(mondayReminder, () =>
   reminder({
